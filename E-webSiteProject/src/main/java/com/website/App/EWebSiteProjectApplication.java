8bfc53175e8d9f7fd8c105fb937080501288ec29
SiddhikNizamiKhan <Siddhikkhan9091@gmail.com>
2022-08-19 12:19:35 +0530

    sdk

---
+++ E-webSiteProject/src/main/java/com/website/App/EWebSiteProjectApplication.java
@@ -0,0 +1,13 @@
+package com.website.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWebSiteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWebSiteProjectApplication.class, args);
+	}
+
+}
